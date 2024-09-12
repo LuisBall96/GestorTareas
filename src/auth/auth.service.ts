eddbeb12b7d91957aa04001a49a08db5cf29bc3f
LuisBall96 <luisballesteros96@hotmail.com>
2024-09-12 14:05:42 -0500

    Auth with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import  * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { NotFoundError } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor( private readonly userService: UsersService){}
+    constructor( 
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+    ){}
 
     async register({ email, password, name }: RegisterDto){
         const user = await this.userService.findOneByEmail(email);
@@ -20,7 +26,23 @@ export class AuthService {
         })
     }
 
-    login(){
-        return 'login'
+    async login({ email, password }: LoginDto){
+
+        const user = await this.userService.findOneByEmail(email);
+
+        if (!user) throw new UnauthorizedException('Email '+ email + " es incorrecto o no existe");
+
+        const passWordIsValid = await bcryptjs.compare(password, user.password);
+
+        if (!passWordIsValid) throw new UnauthorizedException('Contraseña no válida');
+
+        const payload = { email: user.email }
+        
+        const token = await this.jwtService.signAsync(payload)
+
+        return {
+            token,
+            email
+        }
     }
 }
