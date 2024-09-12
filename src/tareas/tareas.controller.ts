628accfac81843c30e2e6f02d93c244c0493abf0
LuisBall96 <luisballesteros96@hotmail.com>
2024-09-12 14:48:54 -0500

    Authorization in crud of Tareas

--- src/tareas/tareas.controller.ts
+++ src/tareas/tareas.controller.ts
@@ -1,33 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TareasService } from './tareas.service';
 import { CreateTareaDto } from './dto/create-tarea.dto';
 import { UpdateTareaDto } from './dto/update-tarea.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('tareas')
 export class TareasController {
   constructor(private readonly tareasService: TareasService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createTareaDto: CreateTareaDto) {
     return this.tareasService.create(createTareaDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.tareasService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.tareasService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateTareaDto: UpdateTareaDto) {
     return this.tareasService.update(id, updateTareaDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.tareasService.remove(id);
   }
